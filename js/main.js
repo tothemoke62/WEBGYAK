2fc4e943284bc0be3ab8ac49768b86f2cd45be27
Krisztina-Haru <kriszti20050325@gmailcom>
2026-05-07 20:16:54 +0200

    fő js fájl módosította: Krisztina

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Hambiii
+const toggle = document.getElementById('menuToggle');
+const nav    = document.getElementById('mainNav');
+
+if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('open');
+    });
+}
