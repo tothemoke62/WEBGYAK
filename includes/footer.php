1a17d860299cbff7becb6a99911d5c2366d39762
Krisztina-Haru <kriszti20050325@gmailcom>
2026-05-07 20:14:27 +0200

    Pizzázó név módosítva módosította: Krisztina

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 
 <footer class="site-footer">
     <div class="footer-inner">
-        <div class="footer-logo">🍕 La <em>Fiamma</em></div>
+        <div class="footer-logo">🍕 La <em>Familia</em></div>
         <p>© <?= date('Y') ?> La Familia Pizzéria – Minden jog fenntartva</p>
         <p class="footer-address">1234 Budapest, Pizza utca 1.</p>
     </div>
