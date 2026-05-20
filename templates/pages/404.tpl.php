b7019fac93931af115d2a703a3f24a59611dad51
Krisztina-Haru <kriszti20050325@gmailcom>
2026-05-20 23:28:31 +0200

    Hiba oldal

---
+++ templates/pages/404.tpl.php
@@ -0,0 +1,8 @@
+<h1 class="page-title">404 <span>–</span> Oldal nem található</h1>
+<p class="page-subtitle">A keresett oldal nem létezik.</p>
+
+<div class="form-card">
+    <p>Sajnáljuk, de a keresett oldal nem található!</p>
+    <br>
+    <a href="index.php" class="btn btn-primary">Vissza a főoldalra</a>
+</div>
